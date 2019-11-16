781eb6db107d57e5aef63aef979d8eb1e70ee22a
Brendan Moore <moorebrendanr@gmail.com>
2019-11-15 22:26:52 -0500

    Add Sign in layout and Contacts List layout

---
+++ app/src/main/java/com/c323proj8/BrendanMoore/ContactsList.java
@@ -0,0 +1,14 @@
+package com.c323proj8.BrendanMoore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_list);
+    }
+}
